311131c4b44cb481a7ef1d3589441e39060f5e50
Nina-mvH <nv5842@princeton.edu>
2026-04-07 13:21:53 -0400

    minor fixes

--- noxfile.py
+++ noxfile.py
@@ -3,14 +3,14 @@ import nox
 nox.options.default_venv_backend = "uv|virtualenv"
 
 
-@nox.session(python=["3.12", "3.13"])
+@nox.session(python=["3.13"])
 def tests(session: nox.Session) -> None:
     session.install("pytest", "uv")
     session.install("-e", ".")
     session.run("pytest", "-v", "tests")
 
 
-@nox.session(python=["3.12"])
+@nox.session
 def lint(session: nox.Session) -> None:
     session.install("pre-commit")
     session.run(
